485d5bab6580c8242ddbd138f2040aa1e900032a
Everardo Medina <emedina@rubixware.com>
2022-07-13 00:10:48 -0500

    Fix wrong content resolver

--- android/src/main/java/com/softmaestri/notification/channel/flutter_notification_channel/FlutterNotificationChannelPlugin.java
+++ android/src/main/java/com/softmaestri/notification/channel/flutter_notification_channel/FlutterNotificationChannelPlugin.java
@@ -74,7 +74,7 @@ public class FlutterNotificationChannelPlugin implements FlutterPlugin, MethodCa
             if (customSound == null) {
               uri = Settings.System.DEFAULT_NOTIFICATION_URI;
             } else {
-              uri = Uri.parse(context.getApplicationContext().ContentResolver.SCHEME_ANDROID_RESOURCE + "://"+ context.getPackageName() + "/raw/" + customSound);
+              uri = Uri.parse(context.getApplicationContext().getContentResolver().SCHEME_ANDROID_RESOURCE + "://"+ context.getPackageName() + "/raw/" + customSound);
             }
             Log.i(TAG, "Sound uri: " + uri.toString() + " \n");
             notificationChannel.setSound(uri, attributes);
